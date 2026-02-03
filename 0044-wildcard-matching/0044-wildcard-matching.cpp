4fe3867c3edeede073faa57f5caa0a15a1dbc58f
Aditya Kumar Tiwari <st108850@gmail.com>
2026-02-03 20:16:56 +0530

    Time: 75 ms (18.38%), Space: 96.6 MB (5.98%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.cpp
+++ 0044-wildcard-matching/0044-wildcard-matching.cpp
@@ -1,42 +1,90 @@
 class Solution {
 public:
+    bool fun(int i, int j, string& s, string& p, vector<vector<int>>& dp) {
+        // base case
 
-bool fun(int i,int j,string &s ,string &p,vector<vector<int>>&dp){
-         //base case
+        if (i < 0 && j < 0)
+            return true;
+        if (i < 0 && j >= 0)
+            return false;
 
-         if(i<0&&j<0)return true;
-         if(i<0&&j>=0)return false;
-    
-         if(i>=0&&j<0){
-               bool check=false;
-               for(int k=0;k<=i;k++){
-                  if(p[k]!='*'){
-                        check=true;
-                        break;
+        if (i >= 0 && j < 0) {
+            bool check = false;
+            for (int k = 0; k <= i; k++) {
+                if (p[k] != '*') {
+                    check = true;
+                    break;
+                }
+            }
+            if (!check)
+                return true;
+            return false;
+        }
+
+        if (dp[i][j] != -1)
+            return dp[i][j];
+
+        if (p[i] == s[j] || p[i] == '?') {
+            return dp[i][j] = fun(i - 1, j - 1, s, p, dp);
+        } else if (p[i] == '*') {
+            return dp[i][j] =
+                       fun(i - 1, j, s, p, dp) || fun(i, j - 1, s, p, dp);
+        } else {
+            return dp[i][j] = false;
+        }
+    }
+
+    bool tabulation(string &s, string &p) {
+        int m = s.size();
+        int n = p.size();
+
+        vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
+
+        //  dp[i][j]=is it posssible to match string p[0...i] to string
+        //  s[0....j]
+
+        dp[0][0] = true;
+        for (int j =1; j <= m; j++){
+            dp[0][j] = false;
+        }
+
+        for (int i = 1; i <= n; i++) {
+            bool check = false;
+            for (int k = 1; k <= i; k++) {
+                if (p[k-1] != '*') {
+                    check = true;
+                    break;
+                }
+            }
+            if(!check)dp[i][0]=true;
+            else dp[i][0]=false;
+        }
+
+        for(int i=1;i<=n;i++)
+        {
+             for(int j=1;j<=m;j++)
+             {
+                  if(p[i-1]==s[j-1]||p[i-1]=='?'){
+                        dp[i][j]=dp[i-1][j-1];
                   }
-               }
-               if(!check)return true;
-               return false;
-         }
+                  else if(p[i-1]=='*'){
+                      dp[i][j]=dp[i-1][j]|dp[i][j-1];
+                  }
+                  else {
+                     dp[i][j]=false;
+                  }
+             }
+        }
 
-         if(dp[i][j]!=-1)return dp[i][j];
-
-         if(p[i]==s[j]||p[i]=='?'){
-                 return dp[i][j]=fun(i-1,j-1,s,p,dp);
-         }
-         else if(p[i]=='*'){
-              return dp[i][j]=fun(i-1,j,s,p,dp)||fun(i,j-1,s,p,dp);
-         }
-         else{
-             return dp[i][j]=false;
-         }
-}
+        return dp[n][m];
+    }
     bool isMatch(string s, string p) {
-            
-                 int m=s.size();
-                 int n=p.size();
-                  
-                 vector<vector<int>>dp(n,vector<int>(m,-1));
-                 return fun(n-1,m-1,s,p,dp);
+
+        int m = s.size();
+        int n = p.size();
+
+        vector<vector<int>> dp(n, vector<int>(m, -1));
+        // return fun(n - 1, m - 1, s, p, dp);
+        return tabulation(s,p);
     }
 };
