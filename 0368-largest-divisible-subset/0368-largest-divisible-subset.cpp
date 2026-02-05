be54d17f80286ff88ed498bb4227ce272aaca550
Aditya Kumar Tiwari <st108850@gmail.com>
2026-02-05 10:46:55 +0530

    Time: 63 ms (5.79%), Space: 67.3 MB (5.25%) - LeetHub

--- 0368-largest-divisible-subset/0368-largest-divisible-subset.cpp
+++ 0368-largest-divisible-subset/0368-largest-divisible-subset.cpp
@@ -1,36 +1,53 @@
 class Solution {
 public:
+
+
     vector<int> largestDivisibleSubset(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
-        int n = nums.size();
+          
+              sort(nums.begin(),nums.end());
+               int n=nums.size();
 
-        vector<vector<int>> dp(n+1, vector<int>(n+1, 0));
+               vector<vector<int>>dp(n+1,vector<int>(n+1,0));
 
-        for(int i = n-1; i >= 0; i--) {
-            for(int prev = -1; prev < i; prev++) {
-                int take = 0;
-                if(prev == -1 || nums[i] % nums[prev] == 0) {
-                    take = 1 + dp[i+1][i+1];
-                }
-                int not_take = dp[i+1][prev+1];
-                dp[i][prev+1] = max(take, not_take);
-            }
-        }
+               for(int prev=1;prev<=n;prev++){
+                    dp[n][prev]=0;
+               }
 
-        int i = 0, prev = -1;
-        vector<int> ans;
+               for(int i=n-1;i>=0;i--)
+               {
+                  for(int prev=-1;prev<i;prev++){
+                          int take=INT_MIN;
+                          if(prev==-1||nums[i]%nums[prev]==0){
+                                 take=1+dp[i+1][i+1];
+                          }
+                          int not_take=dp[i+1][prev+1];
 
-        while(i < n) {
-            if(
-                (prev == -1 || nums[i] % nums[prev] == 0) &&
-                dp[i][prev+1] == 1 + dp[i+1][i+1]
-            ) {
-                ans.push_back(nums[i]);
-                prev = i;
-            }
-            i++;
-        }
+                          dp[i][prev+1]=max(take,not_take);
+                          
+                  }
+               }
 
-        return ans;
+               int i=0;
+               int prev=-1;
+               vector<int>ans;
+
+               while(i<n){
+                    if(prev==-1||nums[i]%nums[prev]==0){
+                        if(dp[i][prev+1]==1+dp[i+1][i+1]){
+                           ans.push_back(nums[i]);
+                           prev=i;
+                           
+                        }
+                        i++;
+                    }
+                    else{
+                         i++;
+                    }
+               }
+
+               return ans;
+               
+
+             
     }
 };
