d308408251bf8d5103652ea6424b867232e52142
Aditya Kumar Tiwari <st108850@gmail.com>
2026-02-05 10:09:55 +0530

    Time: 3 ms (80.82%), Space: 14.1 MB (79.38%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,25 +1,23 @@
 class Solution {
 public:
     int lengthOfLIS(vector<int>& nums) {
-        
-               int n=nums.size();
+          // USING BINARY SEARCH
 
-               vector<vector<int>>dp(n+1,vector<int>(n+1,0));
-               
+            vector<int>temp;
+            temp.push_back(nums[0]);
 
-               for(int i=n-1;i>=0;i--)
-               {
-                   for(int prev=-1;prev<i;prev++){
-                        int take=INT_MIN;
-                        if(prev==-1||nums[i]>nums[prev]){
-                                take=1+dp[i+1][i+1];// prev here is i
-                        }
-                        int not_take=dp[i+1][prev+1];
+            for(int i=1;i<nums.size();i++)
+                 {
+                       if(nums[i]>temp.back()){
+                             temp.push_back(nums[i]);
+                       }
+                       else{
+                           int ind=lower_bound(temp.begin(),temp.end(),nums[i])-temp.begin();
+                           temp[ind]=nums[i];
+                       }
+                 }
 
-                        dp[i][prev+1]=max(take,not_take);//prev+1 beacuse we can't store negative index
-                   }
-               }
+                 return temp.size();
 
-               return dp[0][0];// length of subseqeunce such that subsequecne start from i and prev=-1
     }
 };
