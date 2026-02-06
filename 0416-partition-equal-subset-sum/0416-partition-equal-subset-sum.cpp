7caebb7c9fd65392ad94ae87ce52acb416a43efb
Aditya Kumar Tiwari <st108850@gmail.com>
2026-02-06 11:45:19 +0530

    Time: 456 ms (5%), Space: 460.6 MB (5.02%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -1,30 +1,64 @@
 class Solution {
 public:
-    
-bool fun(int i,vector<int>&nums,int target,int sum,vector<vector<int>>&dp){
-       if(2*target==sum)return true;
+    bool fun(int i, vector<int>& nums, int target, int sum,
+             vector<vector<int>>& dp) {
+        if (2 * target == sum)
+            return true;
 
-       if(i>=nums.size()){
-           if(2*target==sum)return true;
-           return false;
-       }
-        if(dp[i][target]!=-1)return dp[i][target];
-       bool take=false;
-       if(target-nums[i]>=0){  
-             take=fun(i+1,nums,target-nums[i],sum,dp);
-       }
-       bool not_take=fun(i+1,nums,target,sum,dp);
+        if (i >= nums.size()) {
+            if (2 * target == sum)
+                return true;
+            return false;
+        }
+        if (dp[i][target] != -1)
+            return dp[i][target];
+        bool take = false;
+        if (target - nums[i] >= 0) {
+            take = fun(i + 1, nums, target - nums[i], sum, dp);
+        }
+        bool not_take = fun(i + 1, nums, target, sum, dp);
 
-       return dp[i][target]=take|not_take;
-       
-}    
-    bool canPartition(vector<int>& nums) {
+        return dp[i][target] = take | not_take;
+    }
+
+    bool tabulation(vector<int>& nums) {
+        int n = nums.size();
+        int sum = accumulate(nums.begin(), nums.end(), 0);
+
+        vector<vector<int>> dp(n + 1, vector<int>(sum + 1, 0));
         
-               int n=nums.size();
-               int sum=accumulate(nums.begin(),nums.end(),0);
+        for(int i=0;i<=n;i++)
+        {
+               for(int target=0;target<=sum;target++){
+                   if(2*target==sum){
+                      dp[i][target]=1;
+                   }
+               }
+        }
 
-               vector<vector<int>>dp(n+1,vector<int>(sum+1,-1));
-               bool ans=fun(0,nums,sum,sum,dp);
-               return ans;
+        for(int i=n-1;i>=0;i--)
+        {
+                for(int target=0;target<=sum;target++){
+                          int take=0;
+                          if(target-nums[i]>=0){
+                               take=dp[i+1][target-nums[i]];
+                          }
+                          int not_take=dp[i+1][target];
+
+                          dp[i][target]=take|not_take;
+                }
+        }
+
+        return dp[0][sum];
+
+    }
+    bool canPartition(vector<int>& nums) {
+
+        int n = nums.size();
+        int sum = accumulate(nums.begin(), nums.end(), 0);
+
+        vector<vector<int>> dp(n + 1, vector<int>(sum + 1, -1));
+        // bool ans = fun(0, nums, sum, sum, dp);
+        return tabulation(nums);
     }
 };
