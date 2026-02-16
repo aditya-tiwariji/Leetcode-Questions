022ab0862e62312ac6edafd9d3e89dc271649c60
Aditya Kumar Tiwari <st108850@gmail.com>
2026-02-16 13:08:20 +0530

    Time: 99 ms (70.77%), Space: 13.9 MB (28.43%) - LeetHub

--- 0312-burst-balloons/0312-burst-balloons.cpp
+++ 0312-burst-balloons/0312-burst-balloons.cpp
@@ -18,12 +18,43 @@ int fun(int i,int j, vector<int>&nums,vector<vector<int>>&dp){
 
          return dp[i][j]=maxi;
 }
-    int maxCoins(vector<int>& nums) {
-              int n=nums.size();
+
+int tabulation(vector<int>& nums){
+            int n=nums.size();
                 nums.push_back(1);
                 nums.insert(nums.begin(),1);
-               vector<vector<int>>dp(n+1,vector<int>(n+1,-1));
-              int ans=fun(1,n,nums,dp);
-              return ans;
+               vector<vector<int>>dp(n+2,vector<int>(n+2,0));
+
+               for(int i=n;i>=1;i--)
+               {
+                    for(int j=1;j<=n;j++)
+                    {
+                          if(i>j)continue;
+                           int maxi=INT_MIN;
+                           
+                            for(int ind=i;ind<=j;ind++)
+                            {
+                                   int cost=nums[ind]*nums[i-1]*nums[j+1]+
+                                          dp[i][ind-1]+dp[ind+1][j];
+                                           maxi=max(maxi,cost);
+                            }
+
+                            dp[i][j]=maxi;
+                    }
+
+
+               }
+
+               return dp[1][n];
+}
+    int maxCoins(vector<int>& nums) {
+              int n=nums.size();
+            //     nums.push_back(1);
+            //     nums.insert(nums.begin(),1);
+            //    vector<vector<int>>dp(n+1,vector<int>(n+1,-1));
+            //   int ans=fun(1,n,nums,dp);
+            //   return ans;
+
+            return tabulation(nums);
     }
 };
