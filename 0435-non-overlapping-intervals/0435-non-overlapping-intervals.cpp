458eb6ddeb6a5c295605fb9359f3d081d956018d
Aditya Kumar Tiwari <st108850@gmail.com>
2026-03-06 14:09:52 +0530

    Time: 39 ms (81.97%), Space: 94.1 MB (36.79%) - LeetHub

--- 0435-non-overlapping-intervals/0435-non-overlapping-intervals.cpp
+++ 0435-non-overlapping-intervals/0435-non-overlapping-intervals.cpp
@@ -2,21 +2,25 @@ class Solution {
 public:
     int eraseOverlapIntervals(vector<vector<int>>& intervals) {
              
-                vector<pair<int,int>>p;
+                // vector<pair<int,int>>p;
                 int n=intervals.size();
 
-                for(auto it:intervals){
-                      p.push_back({it[1],it[0]});
-                }
+                // for(auto it:intervals){
+                //       p.push_back({it[1],it[0]});
+                // }
 
-                sort(begin(p),end(p));
+                auto cmp=[](vector<int>&a,vector<int>&b){
+                         return a[1]<b[1];
+                };
 
-                int prev_end_day=p[0].first;
+                sort(begin(intervals),end(intervals),cmp);
+
+                int prev_end_day=intervals[0][1];
                 int cnt=1;
-                for(int i=1;i<p.size();i++)
+                for(int i=1;i<intervals.size();i++)
                 {
-                        int start_day=p[i].second;
-                        int end_day=p[i].first;
+                        int start_day=intervals[i][0];
+                        int end_day=intervals[i][1];
 
                         if(start_day>=prev_end_day){
                              prev_end_day=end_day;
