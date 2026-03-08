c304eadc6495df35ab7a2249298f53de1df29447
Aditya Kumar Tiwari <st108850@gmail.com>
2026-03-08 10:32:23 +0530

    Time: 1 ms (27.83%), Space: 13.1 MB (29.99%) - LeetHub

--- 1980-find-unique-binary-string/1980-find-unique-binary-string.cpp
+++ 1980-find-unique-binary-string/1980-find-unique-binary-string.cpp
@@ -5,21 +5,14 @@ public:
             int n=nums.size();
 
             unordered_set<string>seen(nums.begin(),nums.end());
-          string p="";
-          bool f=false;
+
             for(int i=0;i<=n;i++){
                    
                        int req1=n-i;
-                       string temp=string(i,'0')+string(req1,'1');
+                        string temp=string(i,'0')+string(req1,'1');
 
-                      if(seen.count(temp)==0){
-                        p= temp;
-                        f=true;
-                        break;
-                      }
+                      if(seen.count(temp)==0)return temp;
             }
-            
-            if(f)return p;
-            else return "";
+            return "";
     }
 };
