04853bbeec128b3a14ca20deb59d54159d7865d4
Aditya Kumar Tiwari <st108850@gmail.com>
2026-03-20 18:35:32 +0530

    Time: 0 ms (100%), Space: 18 MB (39.11%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -4,27 +4,29 @@ public:
                 
              
                 unordered_map<int,int>mpp;
-                for(auto it:nums)mpp[it]++;
+                for(auto it:nums)mpp[it]++;//O(1) avg 
 
                 priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>pq;
                 
                 for(auto it:mpp)
                 {
-                      pq.push({it.second,it.first});
+                      pq.push({it.second,it.first});// O(logn)
                 }
                 int n=mpp.size();
                 int x=n-k;
                 while(!pq.empty()&&x>0){
-                        pq.pop();
+                        pq.pop();// O(logn)
                         x--;
                 }
                 vector<int>ans;
                 while(!pq.empty())
                 {   
-                    ans.push_back(pq.top().second);
+                    ans.push_back(pq.top().second);//O(logn)
                     pq.pop();
                 }
 
                 return ans;
+
+                //overall tc-->O(logn)
     }
 };
