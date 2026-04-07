94994ff55e4dfb9dfe27dfa30bff01a4a76260bc
Aditya Kumar Tiwari <st108850@gmail.com>
2026-04-07 14:21:16 +0530

    Time: 56 ms (20.69%), Space: 127.4 MB (76.72%) - LeetHub

---
+++ 2069-walking-robot-simulation-ii/2069-walking-robot-simulation-ii.cpp
@@ -0,0 +1,82 @@
+class Robot {
+public:
+    
+    char cur='E';
+    unordered_map<char,char>AC;
+    int x=0;
+    int y=0;
+    int mx=0;
+    int my=0;
+
+    Robot(int width, int height) {
+           mx=width;
+           my=height;
+
+        
+               AC['E']='N';
+               AC['W']='S';
+               AC['N']='W';
+               AC['S']='E';
+    }
+    
+    void step(int num) {
+           
+                int perimeter = 2 * (mx + my - 2);
+    
+                int k=num%perimeter;
+                if (k==0)k=perimeter;
+
+             while(k>0){
+                
+                  if(cur=='E'){
+                      if(x+1==mx){
+                           cur=AC[cur];
+                           continue;
+                      }
+                       x++; 
+                  }
+                  else if(cur=='W'){
+                      if(x-1<0){
+                           cur=AC[cur];
+                           continue;
+                      }
+                      x--;
+                  }
+                  else if(cur=='N'){
+                      if(y+1==my){
+                           cur=AC[cur];
+                           continue;
+                      }
+                       y++;
+                  }
+                 else  if(cur=='S'){
+                      if(y-1<0){
+                           cur=AC[cur];
+                           continue;
+                      }
+                      y--;
+                  }
+                  k--;
+             }
+    }
+    
+    vector<int> getPos() {
+             return {x,y};
+    }
+    
+    string getDir() {
+          
+           if(cur=='E')return "East";
+           else if(cur=='W')return "West";
+           else if(cur=='N')return "North";
+           else return "South";
+    }
+};
+
+/**
+ * Your Robot object will be instantiated and called as such:
+ * Robot* obj = new Robot(width, height);
+ * obj->step(num);
+ * vector<int> param_2 = obj->getPos();
+ * string param_3 = obj->getDir();
+ */
