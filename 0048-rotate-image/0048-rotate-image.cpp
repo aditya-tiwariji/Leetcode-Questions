15eba1a2d533e67f35bc88a52ab6e3cfdcea500e
Aditya Kumar Tiwari <st108850@gmail.com>
2026-05-04 10:57:32 +0530

    Time: 0 ms (100%), Space: 10.1 MB (92.7%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.cpp
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -2,23 +2,17 @@ class Solution {
 public:
     void rotate(vector<vector<int>>& matrix) {
 
-        int n=matrix.size();
-        
-        // transpose nikala
+        int n = matrix.size();
 
-        for(int i=0;i<n-1;i++){
-            for(int j=i+1;j<n;j++){
-                 swap(matrix[i][j],matrix[j][i]);
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = i + 1; j < n; j++) {
+                swap(matrix[i][j], matrix[j][i]);
             }
         }
 
-        //REVERSE KIYA har ek row ko
+        for (int i = 0; i < n; i++) {
 
-        for(int i=0;i<n;i++){
-            // ROW REVERSE HO JAYEGA 
-             reverse(matrix[i].begin(),matrix[i].end());
+            reverse(matrix[i].begin(), matrix[i].end());
         }
-
-        
     }
 };
