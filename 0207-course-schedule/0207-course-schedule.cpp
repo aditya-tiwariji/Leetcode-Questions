31375d3081829a858725d43c05c59b58b7afec58
Aditya Kumar Tiwari <st108850@gmail.com>
2026-06-02 16:05:18 +0530

    Time: 3 ms (75.58%), Space: 20.1 MB (33.07%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,43 +1,41 @@
 class Solution {
 public:
-
-bool dfs( int node, vector<int> adj[], vector<int>&vis,vector<int> &pathVis){
-          vis[node]=1;
-          pathVis[node]=1;
-
-          for(auto it:adj[node]){
-                    if(!vis[it]){
-                         if(dfs(it,adj,vis,pathVis)==true)return true;
-                    }
-                    else{
-                        if(pathVis[it])return true;
-
-                    }
-          }
-
-          pathVis[node]=0;
-          return false;
-
-         
-}
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-                  
-                    int n=numCourses;
-                   vector<int>adj[n+1];
-                   for(auto it:prerequisites){
-                       int u=it[0];
-                       int v=it[1];
+           
+               vector<vector<int>>adj(numCourses);
+               vector<int>indegree(numCourses,0);
 
-                       adj[v].push_back(u);
+               for(auto it:prerequisites){
+                    int u=it[0];
+                    int v=it[1];
+
+                    adj[v].push_back(u);
+                    indegree[u]++;
+
+               }
+
+               queue<int>q;
+               for(int i=0;i<numCourses;i++){
+                      if(indegree[i]==0)q.push(i);
+               }
+
+                vector<int>topo;
+               while(!q.empty()){
+                   int node=q.front();
+                   q.pop();
+
+                   topo.push_back(node);
+
+                   for(auto it:adj[node]){
+                         indegree[it]--;
+                         if(indegree[it]==0){
+                               q.push(it);
+                         }
                    }
+               }
 
-                   vector<int>vis(n,0);
-                   vector<int>pathVis(n,0);
+               return topo.size()==numCourses;
 
-                   for(int i=0;i<n;i++){
-                       if(dfs(i,adj,vis,pathVis)==true)return false;
-                   }
-
-                   return true;
-    } 
+               
+    }
 };
