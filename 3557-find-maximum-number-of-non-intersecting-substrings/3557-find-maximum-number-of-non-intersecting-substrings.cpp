ca556726dde18210aa6ddbf4068023eb6243a151
Aditya Kumar Tiwari <st108850@gmail.com>
2026-06-11 17:18:27 +0530

    Time: 298 ms (6.66%), Space: 119.3 MB (5.45%) - LeetHub

--- 3557-find-maximum-number-of-non-intersecting-substrings/3557-find-maximum-number-of-non-intersecting-substrings.cpp
+++ 3557-find-maximum-number-of-non-intersecting-substrings/3557-find-maximum-number-of-non-intersecting-substrings.cpp
@@ -1,21 +1,20 @@
 class Solution {
 public:
     int maxSubstrings(string word) {
+           int n=word.size();
 
-        map<char, int> mpp;
-        int cnt = 0;
-
-        for (int i = 0; i < word.size(); i++) {
-
-            if (!mpp.count(word[i])) {
-                mpp[word[i]] = i;
+            map<char,int>mpp;
+            int cnt=0;
+            for(int i=0;i<n;i++)
+            {
+                  if(mpp.count(word[i])&&i-mpp[word[i]]+1>=4){
+                         cnt++;
+                         mpp.clear();
+                  }
+                  else if(mpp.count(word[i])==0){
+                       mpp[word[i]]=i;
+                  }
             }
-            else if (i - mpp[word[i]] + 1 >= 4) {
-                cnt++;
-                mpp.clear();         
-            }
-        }
-
-        return cnt;
+            return cnt;
     }
 };
