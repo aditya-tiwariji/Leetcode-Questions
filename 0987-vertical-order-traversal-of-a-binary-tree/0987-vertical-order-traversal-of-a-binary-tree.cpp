a92f88c2131eae78078cbe49c4f4d0cb8195bb67
Aditya Kumar Tiwari <st108850@gmail.com>
2026-06-12 19:33:50 +0530

    Time: 2 ms (49.54%), Space: 16.5 MB (18.34%) - LeetHub

---
+++ 0987-vertical-order-traversal-of-a-binary-tree/0987-vertical-order-traversal-of-a-binary-tree.cpp
@@ -0,0 +1,62 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<vector<int>> verticalTraversal(TreeNode* root) {
+                  
+                     queue<pair<TreeNode*,pair<int,int>>>q;
+                     q.push({root,{0,0}});
+
+                     map<int,map<int,multiset<int>>>mpp;
+
+                     while(!q.empty()){
+                            int n=q.size();
+
+                            for(int i=0;i<n;i++)
+                            {
+                                  TreeNode* node=q.front().first;
+                                  int row=q.front().second.first;
+                                  int col=q.front().second.second;
+
+                                  q.pop();
+
+                                  mpp[col][row].insert(node->val);
+
+                                  if(node->left)q.push({node->left,{row+1,col-1}});
+                                  if(node->right)q.push({node->right,{row+1,col+1}});
+
+
+
+
+                            }
+                     }
+
+                       vector<vector<int>>ans;
+                     for(auto it:mpp){// col
+                               vector<int>temp;
+
+                               for(auto r:it.second){//row 
+                                     
+                                       for(auto x:r.second){
+                                                temp.push_back(x);
+                                       }
+
+                               }
+
+                               ans.push_back(temp);
+
+                               
+                     }
+
+                     return ans;
+    }
+};
