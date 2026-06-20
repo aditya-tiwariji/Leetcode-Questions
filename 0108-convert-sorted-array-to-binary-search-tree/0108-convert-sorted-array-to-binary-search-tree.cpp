fb4a4c5bd849687b1d6863a17f7f13313377b083
Aditya Kumar Tiwari <st108850@gmail.com>
2026-06-20 10:52:03 +0530

    Time: 4 ms (34.73%), Space: 23 MB (54.9%) - LeetHub

---
+++ 0108-convert-sorted-array-to-binary-search-tree/0108-convert-sorted-array-to-binary-search-tree.cpp
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+
+ TreeNode* fun(vector<int>&nums,int i,int j){
+                if(i>j)return NULL;
+
+                int mid=(i+j)/2;
+
+                TreeNode*node=new TreeNode(nums[mid]);
+                node->left=fun(nums,i,mid-1);
+                node->right=fun(nums,mid+1,j);
+                return node;
+ }
+    TreeNode* sortedArrayToBST(vector<int>& nums) {
+                      
+                        int n=nums.size();
+                        
+                        int low=0;
+                        int high=n-1;
+
+                        TreeNode*root=fun(nums,0,n-1);
+                        return root;
+    } 
+};
