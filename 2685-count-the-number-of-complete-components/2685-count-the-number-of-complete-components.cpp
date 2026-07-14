dae0bb54a853aa84e0722b7d04df0f4428c7fc28
Aditya Kumar Tiwari <st108850@gmail.com>
2026-07-14 08:49:23 +0530

    Time: 40 ms (61.76%), Space: 135.2 MB (35.97%) - LeetHub

--- 2685-count-the-number-of-complete-components/2685-count-the-number-of-complete-components.cpp
+++ 2685-count-the-number-of-complete-components/2685-count-the-number-of-complete-components.cpp
@@ -1,40 +1,4 @@
-class DisjointSet{
-           
-    vector<int>rank,parent;
-     public:
 
-     DisjointSet(int n){
-            
-                rank.resize(n+1,0);
-                parent.resize(n+1);
-                for(int i=0;i<n;i++)parent[i]=i;
-                
-     }   
-
-     int findUpar(int node){
-             if(node==parent[node])return node;
-
-             return parent[node]=findUpar(parent[node]);
-     }   
-
-     void unionByrank(int u,int v){
-           int ulp_u=findUpar(u);
-           int ulp_v=findUpar(v);
-
-           if(ulp_u==ulp_v)return ;
-
-           if(rank[ulp_u]<rank[ulp_v]){
-                 parent[ulp_u]=ulp_v;
-           }
-           else if(rank[ulp_v]<rank[ulp_u]){
-               parent[ulp_v]=ulp_u;
-           }
-           else{
-                 parent[ulp_u]=ulp_v;
-                 rank[ulp_v]++;
-           }
-     }
-};
 
 class Solution {
 public:
@@ -55,7 +19,7 @@ public:
     int countCompleteComponents(int n, vector<vector<int>>& edges) {
           
                      
-                     DisjointSet ds(n);
+                   
 
                      vector<vector<int>>adj(n+1);
                  
@@ -63,29 +27,27 @@ public:
                          int u=it[0];
                          int v=it[1];
 
-                         ds.unionByrank(u,v);
+                       
                          adj[u].push_back(v);
                          adj[v].push_back(u);
 
                    }
-               
-                 int cnt=0;
 
                  vector<int>vis(n,0);
-            
-                 for(int i=0;i<n;i++)
-                 {
-                      if(ds.findUpar(i)==i){
-                         
-                           int node=0;
-                           int edges=0;
-                           dfs(i,adj,vis,node,edges);
-                           edges=edges/2;
-                           if(edges==(node*(node-1)/2))cnt++;
-                           
-                      }
-                 }
+                   int cnt=0;
 
+                   for(int i=0;i<n;i++)
+                   {
+                         if(!vis[i]){
+                              int edges=0;
+                              int node=0;
+                                dfs(i,adj,vis,node,edges);
+                                edges=edges/2;
+                                if(edges==(node*(node-1)/2))cnt++;
+                         }
+                   }
+               
+                
                  return cnt;
     }
 };
