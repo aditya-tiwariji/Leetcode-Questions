dabf6dccf40d10a23aa3c02ed6f27411bad9d1fa
Aditya Kumar Tiwari <st108850@gmail.com>
2026-07-16 20:49:37 +0530

    Time: 47 ms (34.74%), Space: 53.1 MB (8.94%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -26,14 +26,46 @@ int fun(int i,vector<int>&coins,int amount,vector<vector<int>>&dp){
 
 
 }
+
+int tabulation(vector<int>&coins,int amount){
+          int n=coins.size();
+
+
+           vector<vector<int>>dp(n+1,vector<int>(amount+1,1e9));
+
+           dp[n][0]=0;
+           
+           for(int i=n-1;i>=0;i--)
+           {
+                 for(int a=0;a<=amount;a++){
+                        
+                        int take=1e9;
+                        if(coins[i]<=a){
+                               take=1+dp[i][a-coins[i]];
+                        }
+                        int not_take=dp[i+1][a];
+
+                        dp[i][a]=min(take,not_take);
+                 }
+           }
+
+           if(dp[0][amount]==1e9)return -1;
+           return dp[0][amount];
+
+           
+}
     int coinChange(vector<int>& coins, int amount) {
         
              int n=coins.size();
             
-              vector<vector<int>>dp(n,vector<int>(amount+1,-1));
-             int ans=fun(0,coins,amount,dp);
+            //   vector<vector<int>>dp(n,vector<int>(amount+1,-1));
+            //  int ans=fun(0,coins,amount,dp);
 
-             return (ans==1e9)?-1:ans;
+            //  return (ans==1e9)?-1:ans;
+
+            int ans=tabulation(coins,amount);
+
+            return ans;
 
              // tc ->>O(n*A)
              //sc-->>O(n*A)
