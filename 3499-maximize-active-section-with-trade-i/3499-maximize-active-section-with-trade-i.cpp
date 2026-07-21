1e33eee16906c038ffe399c742885b22100114b3
Aditya Kumar Tiwari <st108850@gmail.com>
2026-07-21 16:43:41 +0530

    Time: 111 ms (26.12%), Space: 76.9 MB (49.1%) - LeetHub

---
+++ 3499-maximize-active-section-with-trade-i/3499-maximize-active-section-with-trade-i.cpp
@@ -0,0 +1,70 @@
+class Solution {
+public:
+    int maxActiveSectionsAfterTrade(string s) {
+           
+              int one=count(s.begin(),s.end(),'1');
+
+               int cntzl=0;
+               int cntzr=0;
+               int cnt1=0;
+
+            s="1"+s;
+            s=s+"1";
+
+              int n=s.length();
+              int ans=0;
+              int b=0;
+              int k=one;
+              for(int i=0;i<n;i++)
+              {
+                       int j=i;
+                       if(b==0){
+                       while(j<n&&s[j]!='0'){
+                             j++;
+                         
+                             b=1;
+                       }
+                      }
+                        
+                         while(j<n&&s[j]=='0'){
+                              cntzl++;
+                              j++;
+                         }
+
+                         while(j<n&&s[j]=='1'){
+                              cnt1++;
+                              j++;
+                         } 
+
+                         while(j<n&&s[j]=='0'){
+                              cntzr++;
+                              j++;
+                         }
+
+                         if(cntzl>0&&cntzr>0&&cnt1>0){
+                            //   if(cntzl+cntzr+cnt1>ans){
+                            //         ans=cntzl+cntzr+cnt1;
+                            //         k=cnt1;
+                            //   }
+                            //   else if(cntzl+cntzr+cnt1==ans){
+                            //           ans=cntzl+cntzr+cnt1;
+                            //           k=min(k,cnt1);
+                            //   }
+
+                            int sum=cntzr+cnt1+cntzl+one-cnt1;
+                              ans=max(ans,sum);
+                              cntzl=cntzr;
+                              cntzr=0;
+                              cnt1=0;
+                         }
+
+                         i=j;
+                         i--;
+
+
+              }
+
+             
+              return max(ans,one);
+    }
+};
