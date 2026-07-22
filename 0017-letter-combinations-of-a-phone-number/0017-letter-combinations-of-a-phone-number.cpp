1c9fee749010907393be128a2344d55a4022b2fb
Aditya Kumar Tiwari <st108850@gmail.com>
2026-07-22 16:50:27 +0530

    Time: 0 ms (100%), Space: 9.8 MB (7.43%) - LeetHub

---
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
@@ -0,0 +1,71 @@
+class Solution {
+public:
+vector<string>v;
+  unordered_map<char,int>cpp;
+  unordered_map<int,string>mpp;
+
+void fun(int i,string digits,string temp,int n){
+      
+              if(temp.size()==n){
+                  v.push_back(temp);
+                  return ;
+              }
+          
+
+               for(auto c:mpp[digits[i]-'0']){
+                      
+                      if(temp.size()<n){
+                             fun(i+1,digits,temp+c,n);
+                      }
+               }
+          
+          
+}
+    vector<string> letterCombinations(string digits) {
+        
+             
+            
+              int k=2;
+              for(char ch='a';ch<='z';ch++)
+              {
+                       string s="";
+                       int v=0;
+                       if(k!=7&&k!=9)v=1;
+                       char c=ch;
+                       while(v<=3){
+                          s+=c;
+                          cpp[c]=k;
+                          if(v==3)break;
+                           v++;
+                          c++;
+                       }
+                       mpp[k]=s;
+                      
+                       k++;
+                       ch=c;
+
+
+              }
+
+            //   for(auto it:mpp){
+            //        v.push_back(it.second);
+            //   }
+
+            //   return v;
+
+              int n=digits.size();
+            //   string s="";
+            //   for(auto it:digits){
+            //           int x=it-'0';
+            //           s+=mpp[x];
+            //   }
+
+              fun(0,digits,"",n);
+
+           
+
+             
+
+              return v;
+    }
+};
