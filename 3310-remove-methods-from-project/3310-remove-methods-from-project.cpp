38964b63180ced2636dda1a3f1e75658eba5203a
Aditya Kumar Tiwari <st108850@gmail.com>
2026-08-05 16:22:02 +0530

    Time: 560 ms (13.45%), Space: 363 MB (20.35%) - LeetHub

---
+++ 3310-remove-methods-from-project/3310-remove-methods-from-project.cpp
@@ -0,0 +1,69 @@
+class Solution {
+public:
+
+void dfs(int i,  vector<vector<int>>&adj,set<int>&seen){
+              
+             
+               seen.insert(i);
+
+               for(auto it:adj[i]){
+                  if(!seen.count(it)){
+                   dfs(it,adj,seen);
+                  }
+               }
+}
+    vector<int> remainingMethods(int n, int k, vector<vector<int>>& invocations) {
+            
+                    
+                    vector<vector<int>>adj(n);
+
+                    for(auto it:invocations){
+                          int u=it[0];
+                          int v=it[1];
+
+                          adj[u].push_back(v);
+                    }
+
+                    set<int>seen;
+                    seen.insert(k);
+                    for(auto it:adj[k]){
+                            if(!seen.count(it)){
+                               dfs(it,adj,seen);
+                            }
+                    }
+                   
+                    bool f=true;
+                    for(int i=0;i<n;i++){
+                          if(seen.count(i))continue;
+                         
+                          for(auto it:adj[i]){
+                              if(seen.count(it)){
+                                   
+                                      f=false;
+                                      break;
+                              }
+                          }
+                          if(!f)break;
+                    }
+                
+                vector<int>ans;
+                    if(f){
+                          for(int i=0;i<n;i++){
+                              if(seen.count(i)==0){
+                                  ans.push_back(i);
+                              }
+                          }
+                    }
+                    else{
+                          
+                           for(int i=0;i<n;i++){
+                              ans.push_back(i);
+                           }
+
+                    }
+
+                    return ans;
+
+
+    }
+};
