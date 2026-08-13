08e5adbba2ab114e49bce53a095b7f938022c4cb
Aditya Kumar Tiwari <st108850@gmail.com>
2026-08-13 10:15:22 +0530

    Time: 0 ms (100%), Space: 20.9 MB (36.44%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -1,37 +1,46 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
-                  int m=matrix.size();
-                  int n=matrix[0].size();
+           
+            int n=matrix.size();// no of rows
+            int m=matrix[0].size();// no of col
 
-                   vector<int>row(m,0),col(n,0);
+            vector<int>row(n,0),col(m,0);
 
-                   for(int i=0;i<m;i++){
-                      for(int j=0;j<n;j++){
-                           if(matrix[i][j]==0){
-                               row[i]++;
-                               col[j]++;
-                           }
+            for(int i=0;i<n;i++)// O(n)
+            {
+                for(int j=0;j<m;j++)//O(m)
+                {
+                       if(matrix[i][j]==0){
+                               row[i]=1;
+                               col[j]=1;
+
+                       }
+                }
+            }
+
+            for(int i=0;i<n;i++)//O(n)
+            {
+                  if(row[i]==1){
+                      for(int j=0;j<m;j++){//O(m)
+                           matrix[i][j]=0;
                       }
-                   }
+                  }
+            }
 
-                   for(int i=0;i<m;i++){
-                           if(row[i]){
-                               for(int j=0;j<n;j++){
-                                   matrix[i][j]=0;
-                               }
-                           }
-                   }
+            for(int j=0;j<m;j++)//O(m)
+            {
+                  if(col[j]==1){
+                       for(int i=0;i<n;i++){//O(n)
+                            matrix[i][j]=0;
+                       }
+                  }
+            }
 
-                   for(int j=0;j<n;j++){
-                      if(col[j]){
-                           for(int i=0;i<m;i++){
-                               matrix[i][j]=0;
-                           }
-                      }
-                   }
+            // total tc-->O(n*m)
+            // sc-->O(1)
 
-                   
-                   
+
+
     }
 };
