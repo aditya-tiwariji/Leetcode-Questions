60ccca695c3bb8ca4880b1573238cc99c6e3bdbe
Aditya Kumar Tiwari <st108850@gmail.com>
2026-08-13 11:35:09 +0530

    Time: 0 ms (100%), Space: 15.8 MB (13.62%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -3,22 +3,25 @@ public:
     void nextPermutation(vector<int>& nums) {
         int n = nums.size();
 
-        for (int i = n - 2; i >= 0; i--) {
+        for (int i = n - 2; i >= 0; i--) {//O(n)
             if (nums[i] < nums[i + 1]) {
                 int j=n-1;
-                while(j>0){
+                while(j>0){//O(n) but for only one time
                       if(nums[j]>nums[i]){
                            swap(nums[i],nums[j]);
                            break;
                       }
                       j--;
                 }
-                reverse(nums.begin() + i + 1, nums.end());
+                reverse(nums.begin() + i + 1, nums.end());// O(n)
                 
                 return;
             }
         }
 
         reverse(nums.begin(), nums.end());
+
+        // total tc-->O(n)
+        // sc-->O(1)
     }
 };
