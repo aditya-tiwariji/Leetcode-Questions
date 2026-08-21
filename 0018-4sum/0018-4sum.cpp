1102e11478d7e6486780673ffef3484f203a8e39
Aditya Kumar Tiwari <st108850@gmail.com>
2026-08-21 18:21:17 +0530

    Time: 21 ms (64.43%), Space: 17.4 MB (85.79%) - LeetHub

--- 0018-4sum/0018-4sum.cpp
+++ 0018-4sum/0018-4sum.cpp
@@ -7,13 +7,13 @@ using ll =long long;
 
                 ll n=nums.size();
 
-                sort(nums.begin(),nums.end());
+                sort(nums.begin(),nums.end());//O(nlog(n))
                 vector<vector<int>>ans;
 
-                for(ll i=0;i<n-3;i++){
+                for(ll i=0;i<n-3;i++){// O(n)
                       if(i-1>=0&&nums[i]==nums[i-1])continue;
 
-                      for(ll j=i+1;j<n-2;j++){
+                      for(ll j=i+1;j<n-2;j++){//O(n)
                             if(j-1>i&&nums[j]==nums[j-1])continue;
 
                             ll sum=nums[i]+nums[j];
@@ -22,7 +22,7 @@ using ll =long long;
                             ll left=j+1;
                             ll right=n-1;
 
-                            while(left<right){
+                            while(left<right){//O(n)
                                    
                                    ll sum1=nums[left]+nums[right];
                                    if(sum1==req){
@@ -44,6 +44,8 @@ using ll =long long;
                             }
                       }
                 }
+                // total tc-->O(nlogn)+O(n^3)
+                // sc -->O(1)
 
                 return ans;
     }
