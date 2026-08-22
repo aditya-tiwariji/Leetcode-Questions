ecedcf72ce492d6c6f0865127af8423fb2344081
Aditya Kumar Tiwari <st108850@gmail.com>
2026-08-22 12:26:15 +0530

    Time: 59 ms (73.36%), Space: 75 MB (30.46%) - LeetHub

--- 1353-maximum-number-of-events-that-can-be-attended/1353-maximum-number-of-events-that-can-be-attended.cpp
+++ 1353-maximum-number-of-events-that-can-be-attended/1353-maximum-number-of-events-that-can-be-attended.cpp
@@ -1,33 +1,37 @@
 class Solution {
 public:
     int maxEvents(vector<vector<int>>& events) {
-           int n=events.size();
-             
-             sort(events.begin(),events.end());
-          priority_queue<int,vector<int>,greater<int>>pq;
-         
-           int cnt=0;
-           int i=0;
-            for(int day=1;day<=1e5;day++)
-            {
+              
+                 int n=events.size();
+
+                 sort(events.begin(),events.end());
+
+                 priority_queue<int,vector<int>,greater<int>>pq;
+                 int i=0;
+                 int cnt=0;
+
+                 for(int day=1;day<=1e5;day++){
                       
-                       while(i<n&&events[i][0]==day)
-                       {
-                              pq.push(events[i][1]);
-                              i++;
-                       }
-
-                       while(!pq.empty()&&pq.top()<day){
-                               pq.pop();
-                       }
-
-                      if(!pq.empty())
-                      {
-                             cnt++;
-                             pq.pop();
+                      while(i<n&&events[i][0]<=day){
+                            pq.push(events[i][1]);
+                            i++;
                       }
-            }
 
-            return cnt;
+                      while(!pq.empty()&&pq.top()<day){
+                           pq.pop();
+                      }
+
+                      if(!pq.empty()){
+                          pq.pop();
+                          cnt++;
+                      }
+
+                      if(i==n&&pq.empty()){
+                            break;
+                      }
+                         
+                 }
+
+                 return cnt;
     }
 };
