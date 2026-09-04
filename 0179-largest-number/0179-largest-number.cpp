7f2c6555dd3a5abd922262990331df3d5ae47b84
Aditya Kumar Tiwari <st108850@gmail.com>
2026-09-04 17:40:13 +0530

    Time: 3 ms (74.49%), Space: 17.1 MB (56.68%) - LeetHub

--- 0179-largest-number/0179-largest-number.cpp
+++ 0179-largest-number/0179-largest-number.cpp
@@ -1,89 +1,28 @@
 class Solution {
 public:
+    static bool cmp(string str1,string str2)
+    {
+        return str1+str2 > str2+str1;
+    }
+    string largestNumber(vector<int>& nums) {
+        int n = nums.size();
 
+        vector<string>v;
 
-string fun(string a, string b) {
-
-    if (a == "") return b;
-    if (b == "") return a;
-
-    int i = 0;
-    int j = 0;
-    int n = a.size();
-    int m = b.size();
-
-    // Compare a+b and b+a using your character comparison style
-    while (i < n + m && j < n + m) {
-
-        char x, y;
-
-        // Character from a+b
-        if (i < n)
-            x = a[i];
-        else
-            x = b[i - n];
-
-        // Character from b+a
-        if (j < m)
-            y = b[j];
-        else
-            y = a[j - m];
-
-        if (x != y) {
-            if (x > y) {
-                return a + b;
-            }
-            else {
-                return b + a;
-            }
+        for(int i=0;i<n;i++)
+        {
+            string temp = to_string(nums[i]);
+            v.push_back(temp);
         }
 
-        i++;
-        j++;
-    }
+        sort(v.begin(),v.end(),cmp);
 
-    return a + b;
-}
-
-
-string largestNumber(vector<int>& nums) {
-
-    int n = nums.size();
-
-    vector<string> arr;
-
-    for (int i = 0; i < n; i++) {
-
-        string s = to_string(nums[i]);
-
-        bool inserted = false;
-
-        // Find correct position for s
-        for (int j = 0; j < arr.size(); j++) {
-
-            // Your fun() tells us which concatenation is larger
-            if (fun(s, arr[j]) == s + arr[j]) {
-                arr.insert(arr.begin() + j, s);
-                inserted = true;
-                break;
-            }
+        string ans="";
+        for(int i=0;i<n;i++)
+        {
+            ans += v[i];
         }
 
-        if (!inserted) {
-            arr.push_back(s);
-        }
+        return ans[0]=='0'? "0" : ans;
     }
-
-    string p = "";
-
-    for (int i = 0; i < arr.size(); i++) {
-        p += arr[i];
-    }
-
-    if (p[0] == '0') return "0";
-
-    return p;
-}
-
-
 };
