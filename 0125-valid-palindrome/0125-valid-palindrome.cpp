c966256f4690d26da4ee69de5b96b6475048bf6a
Aditya Kumar Tiwari <st108850@gmail.com>
2026-09-20 22:35:39 +0530

    Time: 1 ms (42.72%), Space: 11.5 MB (7.75%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -1,26 +1,36 @@
-class Solution {
-public:
-    bool isPalindrome(string s) {
-            
-            if(s=="")return true;
+class Solution { 
+public: 
+    bool isPalindrome(string s) { 
+         
+        stringstream ss(s); 
+ 
+        string word; 
+        vector<char> ans; 
+ 
+        while(ss >> word) { 
+                    
+            for(auto it : word) { 
+               
 
-            int n=s.size();
-            string ans=""; 
-            for(int i=0;i<n;i++)
-            {
-                   
-                   int val=s[i];
-                   if((val>=65&&val<=90)||(val>=97&&val<=122)||(s[i]>='0'&&s[i]<='9')){
-                          s[i]=tolower(s[i]);
-                          ans+=s[i];
-                   }
+                if(isalnum(it)){
+                     it = tolower(it);
+                     ans.push_back(it);
+                }
             }
+        }
 
-            string p=ans;
+        int i = 0;
+        int j = ans.size() - 1;
 
-            reverse(ans.begin(),ans.end());
+        while(i <= j) {
+                   
+            if(ans[i] != ans[j])
+                return false;
 
-            if(ans==p)return true;
-            return false;
+            i++;
+            j--;
+        }
+
+        return true;
     }
-}; 
+};
